10ae1cb739273ba593aa1e95a50aa80273312ddc
Janireee <71083436+Janireee@users.noreply.github.com>
2023-05-23 11:41:22 +0200

    Cambios en los temas

--- app/src/main/java/com/example/proyecto2/Fragments/FragmentSobreNosotros.java
+++ app/src/main/java/com/example/proyecto2/Fragments/FragmentSobreNosotros.java
@@ -29,16 +29,6 @@ public class FragmentSobreNosotros extends Fragment {
     public FragmentSobreNosotros() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentSobreNosotros.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentSobreNosotros newInstance(String param1, String param2) {
         FragmentSobreNosotros fragment = new FragmentSobreNosotros();
         Bundle args = new Bundle();
